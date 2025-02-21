72616132db8a8f4afff380fc6300ec2c9b4ce456
Samuel Yao <samuelyao@SamueldeMacBook-Air.local>
2025-02-21 17:49:20 -0500

    killed a easy question

--- leetcode/prefix_sum/724.py
+++ leetcode/prefix_sum/724.py
@@ -3,3 +3,19 @@ The key is to calculate the sum of the array before and after i, where we compar
 
 Note that comparing the sum of the array before and after i using 2 pointer doesnt work, since the case where i sits at exactly 0 or len(nums) - 1 does not work
 '''
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        sum_all = sum(nums)
+
+        pivot = 0
+        for i in range(len(nums)):
+            if pivot == sum_all - nums[i] - pivot:
+                return i
+            pivot += nums[i]
+
+        return -1
