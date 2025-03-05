0e69c2a0b158baf6ac65edf9194e1129f7f29616
Samuel Yao <samuelyao@eduroam-campus-10-36-138-246.campus-dynamic.uwaterloo.ca>
2025-03-04 22:34:00 -0500

    finished binary search

--- leetcode/binary_search/875.py
+++ leetcode/binary_search/875.py
@@ -1,34 +1,28 @@
-import math
+'''
+what the fuck: hours_needed = (eat_per_hour + pile - 1) // eat_per_hour
+
+Whenever we are doing binary search
+1. we think about the upper and lower bound of the search
+2. when updating the bound, use brain to analyse
+    its possible that the condition is different than the "mid"
+3. If we do binary search "all the way", the minimum value in the array that fits the requirement will be sitting at "low"
+4. everyone says that "binary search needs to be using to a sorted data"
+    Think whether the current data is "sorted"
+'''
 class Solution(object):
     def minEatingSpeed(self, piles, h):
-        """
-        :type piles: List[int]
-        :type h: int
-        :rtype: int
-        """
-        # pile[i] is ith pile of banana
-        # h is number hours guard gone
-        # return the eats per hour for koko
+        low = 1
+        high = max(piles)
 
-        # Brute force
+        while low <= high:
+            speed_guess = (low + high) // 2
+            # how many hours current eating speed is needed to finish banana
+            curr_hour = sum((pile + speed_guess - 1) // speed_guess for pile in piles)
 
-        # init the return value as 1 since thats the least we can do
-        # we increment this hour by 1 every time when the hours needed for eating a pile banana
-        eat_per_hour = 1
-
-        # Init a variable that records the current hour needed
-        hour_taken = 0
-        while hour_taken != h: # While the current eat_per_hour is not enough
-            hour_taken = 0
-            # An iteration through the current pile
-            for pile in piles
-                # Get the number of hour needed to finish the current pile
-                '''
-                what the fuck
-                '''
-                hours_needed = (eat_per_hour + pile - 1) // eat_per_hour
-                # add it into the eat_per_hour
-                hour_taken += hours_needed
+            if curr_hour > h:
+                # the current speed is too low
+                low = speed_guess + 1
+            else:
+                high = speed_guess - 1
         
-        # if the loop has finished, it means we found the minimum eat_per_hour for given h
-        return eat_per_hour
+        return low
